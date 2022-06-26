72657b8e179c02b6d898b126d46979159a4b8570
mohammad Ali <mohammadharb96170@gmail.com>
2022-06-26 11:18:12 +0300

    creating Toggle component

---
+++ src/components/Toggle.tsx
@@ -0,0 +1,21 @@
+import React from 'react' 
+import { memo } from 'react';
+
+
+interface ToogleProps extends Pick<React.HTMLAttributes<any>,'onClick'|'className'|'style'> { 
+    open ?: boolean
+ }
+ 
+ const Toggle : React.FC<ToogleProps> = ({open,...rest}) => {
+     
+    
+ 
+   return (
+     <div className='toggle-switch ' {...rest} >
+         <span className={`switch ${open && 'open'}`}></span>
+     </div>
+   )
+ }
+ 
+ export default memo(Toggle)
+ 
