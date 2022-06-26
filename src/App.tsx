ef0eb6694d6b50daddc2f85e4c7c671999ae7955
mohammad Ali <mohammadharb96170@gmail.com>
2022-06-26 10:50:19 +0300

    Updating configs files

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import Button from 'components/Button';
 import React from 'react';
 
 function App() {
   return (
     <div className="App">
          <h1>Testing</h1>
+         <Button title='Test Button' loading />
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,15 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors : {
+        primary : {
+          dark : '#3a3b3c',
+          light : '#695CFE',
+          lighter : '#9F97FE'
+        },
+      }
+    },
   },
   plugins: [],
 }
