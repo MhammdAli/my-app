bcaa88762dbd2f0e890388ced594b29f95fe5e79
mohammad Ali <mohammadharb96170@gmail.com>
2022-06-26 16:03:15 +0300

    updating routing repository

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,41 @@
-import Button from 'components/Button';
-import Input from 'components/Input';
-import SideBar from 'components/SideBar';
-import Toggle from 'components/Toggle';
+import SideBar from 'components/SideBar'; 
+import { useTheme } from 'hooks/useTheme';
+import NotFound from 'pages/404';
+import CreatePost from 'pages/CreatePosts';
+import CreateTags from 'pages/CreateTags';
+import CreateUser from 'pages/CreateUser';
+import EditPost from 'pages/EditPost';
+import EditTags from 'pages/EditTags';
+import EditUser from 'pages/EditUser'; 
+import Home from 'pages/Home';
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
-const Home = ()=>{
-  return (
-    <h1>Hello From Home</h1>
-  )
-}
-
-const CreateUser = ()=>{
-  return (
-    <h1>Create User</h1>
-  )
-}
-
-const NotFound = ()=>{
-  return (
-    <h1>Not Found</h1>
-  )
-}
+ 
+ 
 
 function App() {
-  return (
-    <div className="App">
-         <SideBar/>
 
+  const {mode} = useTheme();
+
+  return (
+    <div className={`App ${mode}`}>
+
+         <SideBar/>
+    
          <div className='layout'>
-          <Routes>
-            <Route path='/' element={<Home/>}/>
-            <Route path='/create/user' element={<CreateUser/>}/>
-            <Route path='*' element={<NotFound/>}/>
-          </Routes>
-        </div>
+            <Routes>
+              <Route path='/' element={<Home/>}/>
+              <Route path='/create/user' element={<CreateUser/>}/>
+              <Route path='/create/post' element={<CreatePost/>}/>
+              <Route path='/create/tag' element={<CreateTags/>}/>
+              <Route path='/Edit/tag/:id' element={<EditTags/>}/>
+              <Route path='/Edit/post/:id' element={<EditPost/>}/>
+              <Route path='/Edit/user/:id' element={<EditUser/>}/>
+              <Route path='*' element={<NotFound/>}/>
+            </Routes>
+         </div>
+         
     </div>
   );
 }
