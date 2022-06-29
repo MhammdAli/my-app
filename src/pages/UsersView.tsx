afc7eca2594f07affc8ecf636f7093e4b0d2c746
mohammad Ali <mohammadharb96170@gmail.com>
2022-06-29 09:19:42 +0300

    updating users view

--- src/pages/UsersView.tsx
+++ src/pages/UsersView.tsx
@@ -1,55 +1,95 @@
-import React from 'react'
-import Table from 'rc-table' 
+import React, { useEffect, useMemo, useState } from 'react'
+import Table from 'components/Table' 
 import Button from 'components/Button';
-import Center from 'components/Center';
-
-const columns = [
+import { NavLink } from 'react-router-dom';
+import {toast, ToastContainer} from 'react-toastify';
+import {User,deleteUser, getUsers} from 'services/users'
+const COLS = [
+  {
+    title: 'Id',
+    dataIndex: 'id', 
+  },
   {
     title: 'First Name',
-    dataIndex: 'first_name',
-    key: 'fname',  
+    dataIndex: 'firstName', 
   },
   {
     title: 'last Name',
-    dataIndex: 'last_name',
-    key: 'lname', 
+    dataIndex: 'lastName', 
   },
   {
     title: 'Email',
-    dataIndex: 'email',
-    key: 'email', 
+    dataIndex: 'email', 
   },
   {
     title: 'mobile',
-    dataIndex: 'mobile',
-    key: 'mobile', 
-  },
-  {
-    title: 'Operations',
-    dataIndex: '',
-    key: 'operations',
-    render: () => <div className='space-x-3 flex '>
-      <Button className='p-2 text-sm flex-1 text-center block' href='#'>Update</Button>
-      <Button className='p-2 text-sm flex-1 text-center block' variantColor='danger' href='#'>Delete</Button>
-      <Button className='p-2 text-sm flex-1 text-center block' variantColor='secondary' href='#'>View</Button>
-    </div>,
-  },
+    dataIndex: 'mobile',  
+  } 
 ];
+ 
+ 
+ 
+  function Actions(data : User[],changeData : (data : User[])=>void){
+   
+   
+    return {
+      title: 'Operations',
+      dataIndex: '', 
+      render: (row : User) => { 
+       
+        const handleDelete = async()=>{
+           
+          try{
+            await deleteUser(row.id as string);
+            const newData = data.filter((currentRow : User)=>currentRow.id !== row.id) 
+            changeData(newData);
+            toast.success('user deleted successfuly')
+          }catch(err : any){
+            toast.error(err.message);
+          }
 
-const data = [
-    { first_name: 'Jack', last_name: 'Json', email: 'jack@gmail.com',mobile : '7972837283', key: '1' },
-    { first_name: 'Rose', last_name : 'Mousia', email: 'Rose@gmail.com',mobile : '7972837283', key: '2' },
-    { first_name: 'Jack', last_name: 'Json', email: 'jack@gmail.com', mobile : '7972837283',key: '3' },
-    { first_name: 'Rose', last_name : 'Mousia', email: 'Rose@gmail.com', key: '4' , mobile : '7972837283'},
-  ];
-  
+        }
+
+    
+
+        return (
+          <div className='space-x-3 flex '>
+            <NavLink to={`/edit/user/${row.id}`} className='btn-sm btn primary contained'>Update</NavLink>
+            <Button className='btn-sm' variantColor='danger' href='#' onClick={handleDelete}>Delete</Button> 
+         </div>
+        )
+      }
+    }
+  }
+
 
 const UsersView = () => {
+ 
+
+  const [users,setUsers] = useState<User[]>([]);
+  const columns = useMemo(()=>[...COLS,Actions(users,setUsers)],[users])
+ 
+
+  useEffect(()=>{
+
+    (async()=>{
+        try{
+          const users = await getUsers();
+          setUsers(users)
+        }catch(err : any){
+          toast.error(err.message)
+        }
+    })();
+    
+  },[])
+
+  
   return (
-    <Center className='content-center'>
-        <h1 className='text-5xl dark:text-white'>Users Data</h1>
-       <Table  className='w-full md:w-[77%] m-auto overflow-x-auto' columns={columns} data={data} /> 
-    </Center>
+    <div>
+      <ToastContainer />
+      <Table columns={columns} rows={users} title='Users Data' rowKey='id'/>
+    </div>
+   
   )
 }
 
